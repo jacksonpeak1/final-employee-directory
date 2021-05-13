d2a3355cd35557e5bc75fdc3341b37ffb1e6571d
Jackson Peak <jacksonpeak1@icloud.com>
2021-05-13 15:59:04 -0400

    updated components and added an employee table and searchbar among others

--- src/App.js
+++ src/App.js
@@ -2,30 +2,21 @@ import React, { useState, useEffect } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import API from "./utils/API";
+import SearchBar from "./components/SearchBar";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
 
 function App() {
   useEffect(() => {
     API.findUsers().then((results) => {
-      console.log(results);
+      console.log(results.data.results);
     });
   }, []);
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SearchBar />
     </div>
   );
 }
---
+++ src/components/SearchBar.js
@@ -0,0 +1,8 @@
+import React from "react";
+import EmployeeTable from "./EmployeeTable";
+
+function SearchBar(props) {
+  return <div>Hello I am the search bar!<EmployeeTable /></div>;
+}
+
+export default SearchBar;
